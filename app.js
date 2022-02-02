706c6fc293f9588c1473484c26af76c8a29888f9
GardoKirsipuu <gardo1999@gmail.com>
2022-02-02 14:27:31 +0200

    Node.js NPM

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var fs = require('fs');
-
-fs.unlink('mynewfile3.txt', function (err) {
-  if (err) throw err;
-  console.log('File deleted!');
-});
+var http = require('http');
+var uc = require('upper-case');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(uc.upperCase("Hello World!"));
+  res.end();
+}).listen(8080);
